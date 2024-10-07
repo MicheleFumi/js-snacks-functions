8a59395af3c09c05b65919fcc684a0218b0c41f6
MicheleFumi <michele.fumi96@gmail.com>
2024-10-07 14:31:46 +0200

    add first solution

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,7 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function initals() {
+    first_letter=[];
 
+for (let i = 0; i < names.length; i++) {
+
+    first_letter.push (names[i].charAt(0));
+   
+
+
+}
+    return first_letter
+  
+   
+    
+}
+console.log(initals());
 
 // Invoca la funzione qui e stampa il risultato in console
 
