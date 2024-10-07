f52b9fe44995396251ceb478d1e5737841360219
MicheleFumi <michele.fumi96@gmail.com>
2024-10-07 16:30:25 +0200

    add third solutions

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,24 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function litteral(word) {
+    let array_word= word.split("")
+    let vowel=["a", "e", "i", "o", "u"]
+    let result= [];
+    for (let i = 0; i < word.length; i++) {
 
+       if (vowel.includes(array_word[i])) {
+      result.push(array_word[i])
+        
+       }
+        
+    }
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(litteral(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
