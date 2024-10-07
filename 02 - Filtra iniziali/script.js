9cbd612b4602f10a9393fe7a6983ba11af8e4f13
MicheleFumi <michele.fumi96@gmail.com>
2024-10-07 15:06:08 +0200

    add second partial solution

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,9 +1,20 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
-
+const letter = "A"
 // Dichiara la funzione qui.
+function filter() {
+    for (let i = 0; i < names.length; i++) {
+        let filtered_name=[]
+        
+        filtered_name.push(letter.filter)
+    }
+
+   
+    
+} 
+console.log(filter());
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
